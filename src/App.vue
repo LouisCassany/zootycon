00cfd05ff47bed71c1e8cc27ff9dfa2537e3a36e
LouisCassany <130476356+LouisCassany@users.noreply.github.com>
2026-02-15 14:39:44 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="text-3xl underline">
+    Hello {{ world }}
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue'
+
+const world = ref("world!")
+
+</script>
